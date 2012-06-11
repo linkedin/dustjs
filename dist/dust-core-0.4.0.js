5e66ef127aea7e91b6085cced9d6ed2b5ab8ce92
Jairo de Morais <jairodemorais@gmail.com>
2012-06-11 15:37:43 -0300

    ws relaxed to allow eol

--- dist/dust-core-0.4.0.js
+++ dist/dust-core-0.4.0.js
@@ -540,7 +540,54 @@ if (typeof exports !== "undefined") {
   module.exports = dust;
 }
 (function(dust){
+
+function isSelect(context) {
+  var value = context.current();
+  return typeof value === "object" && value.isSelect === true;    
+}
+
+function filter(chunk, context, bodies, params, filter) {
+  var params = params || {},
+      actual, expected;
   
+  if (params.key) {
+    actual = context.get(params.key);
+  } else if (isSelect(context)) {
+    actual = context.current().value;
+    if (context.current().isResolved) {
+      filter = function() { return false; };
+    }
+  } else {
+    throw "No key specified for filter and no key found in context from select statement";
+  }
+
+  expected = params.value;
+  if (filter(expected, coerce(actual, params.type, context))) {
+    if (isSelect(context)) {
+      context.current().isResolved = true;
+    }
+    return chunk.render(bodies.block, context);
+  } else if (bodies['else']) {
+    return chunk.render(bodies['else'], context);
+  }
+
+  return chunk.write('');
+}
+
+function coerce (value, type, context) {
+  if (value) {
+    switch (type || typeof(value)) {
+      case 'number': return +value;
+      case 'string': return String(value);
+      case 'boolean': return Boolean(value);
+      case 'date': return new Date(value);
+      case 'context': return context.get(value);
+    }
+  }
+
+  return value;
+}
+
 var helpers = {
   
   sep: function(chunk, context, bodies) {
@@ -553,11 +600,11 @@ var helpers = {
   idx: function(chunk, context, bodies) {
     return bodies.block(chunk, context.push(context.stack.index));
   },
-  
+
   "if": function( chunk, context, bodies, params ){
-    var cond = ( params.cond );
-    
     if( params && params.cond ){
+      var cond = params.cond;
+
       // resolve dust references in the expression
       if( typeof cond === "function" ){
         cond = '';
@@ -572,18 +619,46 @@ var helpers = {
       // eval expressions with no dust references
       if( eval( cond ) ){
        return chunk.render( bodies.block, context );
-      } 
+      }
       if( bodies['else'] ){
        return chunk.render( bodies['else'], context );
       }
-    } 
+    }
     // no condition
     else {
-      if( window.console ){
+      if( typeof window !== 'undefined' && window.console ){
         window.console.log( "No expression given!" );
       }
     }
     return chunk;
+  },
+
+  select: function(chunk, context, bodies, params) {
+    return chunk.render(bodies.block, context.push({ isSelect: true, isResolved: false, value: context.get(params.key) }));
+  },
+
+  eq: function(chunk, context, bodies, params) {
+    return filter(chunk, context, bodies, params, function(expected, actual) { return actual === expected; });
+  },
+
+  lt: function(chunk, context, bodies, params) {
+    return filter(chunk, context, bodies, params, function(expected, actual) { return actual < expected; });
+  },
+
+  lte: function(chunk, context, bodies, params) {
+    return filter(chunk, context, bodies, params, function(expected, actual) { return actual <= expected; });
+  },
+
+  gt: function(chunk, context, bodies, params) {
+    return filter(chunk, context, bodies, params, function(expected, actual) { return actual > expected; });
+  },
+
+  gte: function(chunk, context, bodies, params) {
+    return filter(chunk, context, bodies, params, function(expected, actual) { return actual >= expected; });
+  },
+
+  "else": function(chunk, context, bodies, params) {
+    return filter(chunk, context, bodies, params, function(expected, actual) { return true; });
   }
 };
 
