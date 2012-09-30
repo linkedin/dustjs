2030f2fd8cb8edb3f59d5906a688b45e49481759
vybs <veena.braj@gmail.com>
2012-09-30 14:43:28 -0700

    Add unit tests for the pull/154

--- test/jasmine-test/spec/coreTests.js
+++ test/jasmine-test/spec/coreTests.js
@@ -42,6 +42,20 @@ var coreTests = [
     message: "should test a basic replace"
   },
   {
+    name:     "partial_with_blocks",
+    source:   "{+header}default header {/header}Hello {name}! You have {count} new messages.",
+    context:  { name: "Mick", count: 30 },
+    expected: "default header Hello Mick! You have 30 new messages.",
+    message: "should test a partial with blocks"
+  },
+  {
+    name:     "partial_with_blocks_and_no_defaults",
+    source:   "{+header/}Hello {name}! You have {count} new messages.",
+    context:  { name: "Mick", count: 30 },
+    expected: "Hello Mick! You have 30 new messages.",
+    message: "should test a partial with blocks and no defaults"
+  },
+  {
     name:     "false value in context is treated as empty, same as undefined",
     source:   "{false}",
     context:  { "false": false },
@@ -464,14 +478,35 @@ var coreTests = [
     source:   '{>replace/} {>"plain"/} {>"{ref}"/}',
     context:  { name: "Jim", count: 42, ref: "plain" },
     expected: "Hello Jim! You have 42 new messages. Hello World! Hello World!",
-    message: "should test partials"
+    message:  "should test partials"
   },
   {
     name:     "partial with context",
     source:   "{>replace:.profile/}",
     context:  { profile: { name: "Mick", count: 30 } },
     expected: "Hello Mick! You have 30 new messages.",
-    message: "should test partial with context"
+    message:  "should test partial with context"
+  },
+  {
+    name:     "partial with blocks, with no default values for blocks",
+    source:   '{>partial_with_blocks_and_no_defaults/}',
+    context:  { name: "Mick", count: 30 },
+    expected: "Hello Mick! You have 30 new messages.",
+    message:  "partial with blocks, with no default values for blocks"
+  },
+  {
+    name:     "partial with blocks, with no default values for blocks, but override with globals",
+    source:   '{>partial_with_blocks_and_no_defaults/}{<header}override header {/header}',
+    context:  { name: "Mick", count: 30 },
+    expected: "override header Hello Mick! You have 30 new messages.",
+    message:  "partial with blocks, with no default values for blocks, but override with globals"
+  },
+  {
+    name:     "partial with blocks, overriden with globals",
+    source:   '{>partial_with_blocks/}{<header}my header {/header}',
+    context:  { name: "Mick", count: 30 },
+    expected: "my header Hello Mick! You have 30 new messages.",
+    message: "partial with blocks, overriden with globals"
   },
   {
     name:     "partial with inline params",
@@ -479,6 +514,20 @@ var coreTests = [
     context:  { n: "Mick", c: 30 },
     expected: "Hello Mick! You have 30 new messages.",
     message: "should test partial with inline params"
+  },
+  {
+    name:     "partial with blocks and inline params",
+    source:   '{>partial_with_blocks name=n count="{c}"/}',
+    context:  { n: "Mick", c: 30 },
+    expected: "default header Hello Mick! You have 30 new messages.",
+    message: "should test partial with blocks and inline params"
+  },
+  {
+    name:     "partial with blocks, override default values for blocks and inline params",
+    source:   '{>partial_with_blocks name=n count="{c}"/}{<header}my header {/header}',
+    context:  { n: "Mick", c: 30 },
+    expected: "my header Hello Mick! You have 30 new messages.",
+    message: "should test partial with blocks, override default values for blocks and inline params"
   },
   {
     name:     "partial with inline params tree walk up",
