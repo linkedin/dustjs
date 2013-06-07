3fe511384758a3d56d4b5213c6b6507a8b0aa1dd
Tom Carchrae <tom@intellecti.ca>
2013-06-07 15:52:50 -0700

    tests of lambda path resolution

--- test/jasmine-test/spec/coreTests.js
+++ test/jasmine-test/spec/coreTests.js
@@ -986,7 +986,51 @@ var coreTests = [
                   },
         expected: "Hello Foo Bar World!",
         message: "should test scope of context function"
-      }
+      },
+      {
+          name:     "test that function returning object is resolved",
+          source:   "Hello {#foo}{bar}{/foo} World!",
+          context:  {
+                      foo: {
+                        foobar: 'Foo Bar',
+                        bar: function () {
+                          return this.foobar;
+                        }
+                      }
+                    },
+          expected: "Hello Foo Bar World!",
+          message: "should test scope of context function"
+        },
+        {
+            name:     "test that nested object result from lambda resolves",
+            source:   "Hello {lambda.lambda.ding.dong} World!",
+            context:  {
+                        lambda : {
+                          lambda : function () {
+                            return { 
+                            	ding : {
+                            		dong : 'Foo Bar'
+                            	}
+                              };
+                            }
+                        }
+                      },
+            expected: "Hello Foo Bar World!",
+            message: "should resolve paths with lambdas in scope of context function"
+        },
+        {
+          name:     "test that nested lambda result resolve",
+          source:   "Hello {ding.dong} World!",
+          context:  {
+                      ding : {
+                        dong: function () {
+                          return 'Foo Bar';
+                        }
+                      }
+                    },
+          expected: "Hello Foo Bar World!",
+          message: "should test scope of context function"
+        }
     ]
   },
   {
