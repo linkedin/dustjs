0d70d347d2bb2d093708f5915c03ad00ba94941e
Prayrit Jain <prjain@prjain-ld.linkedin.biz>
2013-07-23 14:01:30 -0700

    add dust debug stripper and the built dust files

--- dist/dust-full-debug-2.0.0.js
+++ dist/dust-full-debug-2.0.0.js
@@ -15,24 +15,51 @@ function getGlobal(){
 }
 
 (function(dust) {
+/* DEBUG */
 
+var Log = {};
+
+Log.queue = [];
+
+Log.readQueue = function() { return Log.queue; }
+
+Log.clearQueue = function() { Log.queue = []; }
+
+Log.addMessage = function(message, type) {
+  type = type || 'INFO';
+  Log.queue.push({message: message, type: type});
+};
+
+if(!dust) {
+  Log.addMessage('dust is not defined', 'ERROR');
+  return;
+}
+
+/* ENDDEBUG */
 dust.helpers = {};
 
 dust.cache = {};
 
 dust.register = function(name, tmpl) {
   if (!name) {
-    
+    /* DEBUG */ Log.addMessage('template name is undefined', 'WARN'); /* ENDDEBUG */
     return;
   }
-  
+  /* DEBUG */
+  if(typeof tmpl !== 'function') {
+    Log.addMessage('template [' + name + '] cannot be resolved to a Dust function', 'WARN'); 
+  }/* ENDDEBUG */
   dust.cache[name] = tmpl;
 };
 
 dust.render = function(name, context, callback) {
   var chunk = new Stub(callback).head,
       loadedChunk = dust.load(name, chunk, Context.wrap(context, name));
-  
+  /* DEBUG */ 
+  // catch errors from dust.load
+  if(loadedChunk.error && loadedChunk.error.message) {
+    Log.addMessage(loadedChunk.error.message, 'WARN'); 
+  }/* ENDDEBUG */
   loadedChunk.end();
 };
 
@@ -40,7 +67,11 @@ dust.stream = function(name, context) {
   var stream = new Stream();
   dust.nextTick(function() {
     var loadedChunk = dust.load(name, stream.head, Context.wrap(context, name));
-    
+    /* DEBUG */
+    // catch errors from dust.load
+    if(loadedChunk.error && loadedChunk.error.message) {
+      Log.addMessage(loadedChunk.error.message, 'WARN'); 
+    }/* ENDDEBUG */
     loadedChunk.end();
   });
   return stream;
@@ -57,7 +88,10 @@ dust.compileFn = function(source, name) {
     dust.nextTick(function() {
       if(typeof tmpl === 'function') {
         tmpl(master.head, Context.wrap(context, name)).end();
-      }
+      }/* DEBUG */ 
+      else {
+        Log.addMessage('template [' + name + '] cannot be resolved to a dust function', 'WARN'); 
+      }/* ENDDEBUG */
     });
     return master;
   };
@@ -116,11 +150,14 @@ dust.filter = function(string, auto, filters) {
       var name = filters[i];
       if (name === "s") {
         auto = null;
-        
+        /* DEBUG */ Log.addMessage('using unescape filter on [' + string + ']', 'DEBUG'); /* ENDDEBUG */
       }
       else if (typeof dust.filters[name] === 'function') {
         string = dust.filters[name](string);
-      }
+      }/* DEBUG */ 
+      else {
+        Log.addMessage('invalid filter [' + name + ']', 'WARN');
+      }/* ENDDEBUG */
     }
   }
   // by default always apply the h filter, unless asked to unescape with |s
@@ -135,8 +172,8 @@ dust.filters = {
   j: function(value) { return dust.escapeJs(value); },
   u: encodeURI,
   uc: encodeURIComponent,
-  js: function(value) { if (!JSON) {return value; } return JSON.stringify(value); },
-  jp: function(value) { if (!JSON) {return value; } return JSON.parse(value); }
+  js: function(value) { if (!JSON) {/* DEBUG */ Log.addMessage('JSON is undefined.  JSON stringify has not been used on [' + value + ']', 'WARN'); /* ENDDEBUG */return value; } return JSON.stringify(value); },
+  jp: function(value) { if (!JSON) {/* DEBUG */ Log.addMessage('JSON is undefined.  JSON parse has not been used on [' + value + ']', 'WARN'); /* ENDDEBUG */return value; } return JSON.parse(value); }
 };
 
 function Context(stack, global, blocks) {
@@ -166,14 +203,21 @@ Context.prototype.get = function(key) {
     if (ctx.isObject) {
       if(ctx.head) {
         value = ctx.head[key];
-      }
+      }/* DEBUG */ 
+      else {
+        Log.addMessage('context head is undefined for[' + key + ']', 'WARN');
+      }/* ENDDEBUG */
       if (!(value === undefined)) {
         return value;
       }
+    }/* DEBUG */ 
+    else {
+      Log.addMessage('current context is not an object.  Cannot find value for [{' + key + '}]', 'DEBUG');
     }
+    Log.addMessage('Looking for [{' + key + '}] up the context stack', 'DEBUG'); /* ENDDEBUG */
     ctx = ctx.tail;
   }
-  
+  /* DEBUG */ Log.addMessage('Looking for [{' + key + '}] in the globals', 'DEBUG'); /* ENDDEBUG */
   return this.global ? this.global[key] : undefined;
 };
 
@@ -182,7 +226,13 @@ Context.prototype.getPath = function(cur, down) {
   var ctx = this.stack, ctxThis,
       len = down ? down.length : 0,      
       tail = cur ? undefined : this.stack.tail;
-  
+  /* DEBUG */ 
+  if(!down) {
+    Log.addMessage('array parameter in getPath is not defined', 'INFO');
+  }
+  if(!dust.isArray(down)) {
+    Log.addMessage('array parameter in getPath is not an array', 'INFO');
+  }/* ENDDEBUG */
 
   if (cur && len === 0) return ctx.head;
   ctx = ctx.head;
@@ -223,7 +273,7 @@ Context.prototype.push = function(head, idx, len) {
     return context;
   }
   else {
-    
+    /* DEBUG */ Log.addMessage('Head [' + head + '] could not be resolved to a context for the template [' + this.global.__template_name__ + ']', 'WARN'); /* ENDDEBUG */
     return Context.wrap(context);
   }
 };
@@ -234,7 +284,7 @@ Context.prototype.rebase = function(head) {
     return context;
   }
   else {
-    
+    /* DEBUG */ Log.addMessage('Head [' + head + '] could not be resolved to a context for the template [' + this.global.__template_name__ + ']', 'WARN'); /* ENDDEBUG */
     return Context.wrap(context);
   }
 };
@@ -252,7 +302,7 @@ Context.prototype.getBlock = function(key, chk, ctx) {
   var blocks = this.blocks;
 
   if (!blocks) {
-    
+    /* DEBUG */ Log.addMessage('no blocks defined for function getBlock', 'DEBUG'); /* ENDDEBUG */
     return;
   }
   var len = blocks.length, fn;
@@ -284,7 +334,7 @@ function Stack(head, tail, idx, len) {
     this.head = head;
   }
   else {
-    
+    /* DEBUG */ Log.addMessage('head was undefined. Defaulting to {}', 'DEBUG'); /* ENDDEBUG */
     this.head = {};
   }
   this.index = idx;
@@ -298,15 +348,20 @@ function Stub(callback) {
 }
 
 Stub.prototype.flush = function() {
-  var chunk = this.head;
+  var chunk = this.head/* DEBUG */,
+      messages = []/* ENDDEBUG */;
 
   while (chunk) {
-    
+    /* DEBUG */
+    chunk.setLog(Log.readQueue());
+    Log.clearQueue();
+    messages = messages.concat(chunk.log);
+    /* ENDDEBUG */
     if (chunk.flushable) {
       this.out += chunk.data.join(""); //ie7 perf
     } else if (chunk.error) {
-      this.callback(chunk.error);
-      
+      this.callback(chunk.error/* DEBUG */, null, log/* ENDDEBUG */);
+      /* DEBUG */ Log.addMessage('chunk error [' + chunk.error + '] thrown. Ceasing to render this template.', 'WARN'); /* ENDDEBUG */
       this.flush = function() {};
       return;
     } else {
@@ -315,7 +370,7 @@ Stub.prototype.flush = function() {
     chunk = chunk.next;
     this.head = chunk;
   }
-  this.callback(null, this.out);
+  this.callback(null, this.out/* DEBUG */, messages/* ENDDEBUG */);
 };
 
 function Stream() {
@@ -323,16 +378,26 @@ function Stream() {
 }
 
 Stream.prototype.flush = function() {
-  var chunk = this.head;
+  var chunk = this.head/* DEBUG */,
+      messages = []/* ENDDEBUG */;
 
   while(chunk) {
-    
+    /* DEBUG */
+    chunk.setLog(Log.readQueue());
+    Log.clearQueue();
+    messages = messages.concat(chunk.log);
+    /* ENDDEBUG */
     if (chunk.flushable) {
       this.emit('data', chunk.data.join("")); //ie7 perf
-      
+      /* DEBUG */
+      this.emit('log', messages);
+      /* ENDDEBUG */
     } else if (chunk.error) {
       this.emit('error', chunk.error);
-      
+      /* DEBUG */
+      Log.addMessage('chunk error [' + chunk.error + '] thrown. Ceasing to render this template.', 'WARN');
+      this.emit('log', messages);
+      /* ENDDEBUG */
       this.flush = function() {};
       return;
     } else {
@@ -346,12 +411,12 @@ Stream.prototype.flush = function() {
 
 Stream.prototype.emit = function(type, data) {
   if (!this.events) { 
-    
+    /* DEBUG */ Log.addMessage('no events to emit', 'INFO'); /* ENDDEBUG */
     return false;
   }
   var handler = this.events[type];
   if (!handler) {
-    
+    /* DEBUG */ Log.addMessage('Event type [' + type + '] does not exist', 'WARN'); /* ENDDEBUG */
     return false;
   }
   if (typeof handler == 'function') {
@@ -361,7 +426,10 @@ Stream.prototype.emit = function(type, data) {
     for (var i = 0, l = listeners.length; i < l; i++) {
       listeners[i](data);
     }
-  }
+  }/* DEBUG */
+  else {
+    Log.addMessage('handler [' + handler + '] is not of a type that is handled by emit', 'DEBUG');
+  } /* ENDDEBUG */
 };
 
 Stream.prototype.on = function(type, callback) {
@@ -369,10 +437,12 @@ Stream.prototype.on = function(type, callback) {
     this.events = {};
   }
   if (!this.events[type]) {
-    
+    /* DEBUG */ Log.addMessage('event type [' + type + '] does not exist. using just the specified callback.', 'WARN'); /* ENDDEBUG */
     if(callback) {
       this.events[type] = callback;
-    } 
+    } /* DEBUG */ else {
+      Log.addMessage('callback for type [' + type + '] does not exist. listener not registered.', 'WARN'); 
+    } /* ENDDEBUG */
   } else if(typeof this.events[type] === 'function') {
     this.events[type] = [this.events[type], callback];
   } else {
@@ -388,7 +458,9 @@ Stream.prototype.pipe = function(stream) {
     stream.end();
   }).on("error", function(err) {
     stream.error(err);
-  });
+  })/* DEBUG */.on("log", function(logs) {
+    stream.log(logs);
+  })/* ENDDEBUG */;
   return this;
 };
 
@@ -462,7 +534,7 @@ Chunk.prototype.reference = function(elem, context, auto, filters) {
   if (!dust.isEmpty(elem)) {
     return this.write(dust.filter(elem, auto, filters));
   } else {
-    
+    /* DEBUG */ Log.addMessage('reference for element was not found.', 'INFO'); /* ENDDEBUG */
     return this;
   }
 };
@@ -532,7 +604,7 @@ Chunk.prototype.section = function(elem, context, bodies, params) {
   } else if (skip) {
      return skip(this, context);
   }
-    
+  /* DEBUG */ Log.addMessage('can not handle the element given for the section tag', 'WARN'); /* ENDDEBUG */  
   return this;
 };
 
@@ -545,7 +617,7 @@ Chunk.prototype.exists = function(elem, context, bodies) {
   } else if (skip) {
     return skip(this, context);
   }
-  
+  /* DEBUG */ Log.addMessage('not rendering body or skip for exists check', 'DEBUG'); /* ENDDEBUG */
   return this;
 };
 
@@ -558,7 +630,7 @@ Chunk.prototype.notexists = function(elem, context, bodies) {
   } else if (skip) {
     return skip(this, context);
   }
-  
+  /* DEBUG */ Log.addMessage('not rendering body or skip for not exists check', 'DEBUG'); /* ENDDEBUG */
   return this;
 };
 
@@ -616,7 +688,7 @@ Chunk.prototype.helper = function(name, context, bodies, params) {
   if( dust.helpers[name]){
    return dust.helpers[name](this, context, bodies, params);
   } else {
-    
+    /* DEBUG */ Log.addMessage('invalid helper [' + name + ']', 'ERROR'); /* ENDDEBUG */
     return this;
   }
 };
@@ -634,7 +706,13 @@ Chunk.prototype.capture = function(body, context, callback) {
   });
 };
 
+/* DEBUG */
+Chunk.prototype.setLog = function(logQueue) {
+  this.log = logQueue;
+  return this;
+};
 
+/* ENDDEBUG */
 Chunk.prototype.setError = function(err) {
   this.error = err;
   this.root.flush();
---
+++ utils/debug_strip.js
@@ -0,0 +1,9 @@
+var fs   = require('fs'),
+    path = require('path'),
+    root = path.join(path.dirname(__filename), "..");
+
+var orig_code = fs.readFileSync(path.join(root, process.argv[2]), 'utf8');
+
+var final_code = orig_code.replace(/\/\* DEBUG[\s\S]*?\/\*\ ENDDEBUG \*\//g, '');
+    
+fs.writeFileSync(path.join(root, process.argv[3]),final_code);
