c907582771ee3eef6d57cef3429c30ce3194e9cc
Jimmy Chan <jimmyhchan@gmail.com>
2013-12-04 11:38:39 -0800

    move jshintrc from grunt to a real jshintrc file

--- Gruntfile.js
+++ Gruntfile.js
@@ -119,23 +119,7 @@ module.exports = function(grunt) {
     },
     jshint: {
       options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: false,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true,
-          require: true,
-          module: true
-        }
+        jshintrc: true
       },
       gruntfile: {
         src: 'Gruntfile.js'
