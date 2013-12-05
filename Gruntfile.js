de1b6cc6b222d9796a8ffcb8663f956f065d25fb
Jimmy Chan <jimmyhchan@gmail.com>
2013-12-04 17:42:09 -0800

    turn grunt bump on for reals for use with the next bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -163,9 +163,7 @@ module.exports = function(grunt) {
       options: {
         files: ['package.json', 'bower.json'],
         updateConfigs: ['pkg'],
-        push: false,
-        createTag: false,
-        commit: false
+        push: false
       }
     },
     log: {
@@ -218,8 +216,8 @@ module.exports = function(grunt) {
   grunt.registerTask('testClient', ['build', 'jasmine:allTests:build', 'log:testClient', 'connect:testServer']);
 
   grunt.registerTask('copyForRelease', ['clean:dist', 'copy:core', 'copy:coreMin', 'copy:full', 'copy:fullMin', 'copy:license', 'log:copyForRelease']);
-  grunt.registerTask('releasePatch', ['bump-only:patch', 'test', 'copyForRelease', 'compress', /*'bump-commit:patch',*/ 'log:release']);
-  grunt.registerTask('releaseMinor', ['bump-only:minor', 'test', 'copyForRelease', 'compress', /*'bump-commit:minor',*/ 'log:release']);
+  grunt.registerTask('releasePatch', ['bump-only:patch', 'test', 'copyForRelease', 'compress', 'bump-commit:patch', 'log:release']);
+  grunt.registerTask('releaseMinor', ['bump-only:minor', 'test', 'copyForRelease', 'compress', 'bump-commit:minor', 'log:release']);
   // major release should probably be done with care
   // grunt.registerTask('releaseMajor', ['test', 'copyForRelease', 'bump:major', 'log:release']);
 
