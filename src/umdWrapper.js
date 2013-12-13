79c79be5e7dd122f90030e71cd981c6c5a8f4aa8
Jimmy Chan <jimmyhchan@gmail.com>
2013-12-13 09:29:38 -0800

    no need to reference window

--- src/umdWrapper.js
+++ src/umdWrapper.js
@@ -1,13 +1,16 @@
 (function(root, factory) {
   if (typeof exports === 'object') {
-    module.exports = factory();
+    module.exports = factory(require('./dust'));
     // @see server file for parser methods exposed in node
   } else {
-    // expose parser methods to the browser
-    root.dust.parse = factory().parse;
+    var parser = factory(root.dust);
   }
-}(this, function() {
+}(this, function(dust) {
   var parser = 'PARSER_CODE_HERE';
+
+  // expose parser methods
+  dust.parse = parser.parse;
+
   return parser;
 }));
   
