3c5f96690040a662adcd5fae8595677648103df0
Prayrit Jain <prjain@prjain-ld.linkedin.biz>
2014-09-09 16:11:47 -0700

    Release v2.4.2

--- dist/dust-core.js
+++ dist/dust-core.js
@@ -1,4 +1,4 @@
-/*! Dust - Asynchronous Templating - v2.4.1
+/*! Dust - Asynchronous Templating - v2.4.2
 * http://linkedin.github.io/dustjs/
 * Copyright (c) 2014 Aleksander Williams; Released under the MIT License */
 (function(root) {
@@ -346,7 +346,8 @@
         try {
           return ctx.apply(ctxThis, arguments);
         } catch (err) {
-          return dust.log(err, ERROR);
+          dust.log(err, ERROR);
+          throw err;
         }
       };
     } else {
@@ -564,7 +565,12 @@
 
     this.next = branch;
     this.flushable = true;
-    callback(branch);
+    try {
+      callback(branch);
+    } catch(e) {
+      dust.log(e, ERROR);
+      branch.setError(e);
+    }
     return cursor;
   };
 
@@ -607,7 +613,12 @@
   Chunk.prototype.section = function(elem, context, bodies, params) {
     // anonymous functions
     if (typeof elem === 'function') {
-      elem = elem.apply(context.current(), [this, context, bodies, params]);
+      try {
+        elem = elem.apply(context.current(), [this, context, bodies, params]);
+      } catch(e) {
+        dust.log(e, ERROR);
+        return this.setError(e);
+      }
       // functions that return chunks are assumed to have handled the body and/or have modified the chunk
       // use that return value as the current chunk and go to the next method in the chain
       if (elem instanceof Chunk) {
@@ -754,15 +765,15 @@
   Chunk.prototype.helper = function(name, context, bodies, params) {
     var chunk = this;
     // handle invalid helpers, similar to invalid filters
-    try {
-      if(dust.helpers[name]) {
+    if(dust.helpers[name]) {
+      try {
         return dust.helpers[name](chunk, context, bodies, params);
-      } else {
-        dust.log('Invalid helper [' + name + ']', WARN);
-        return chunk;
+      } catch(e) {
+        dust.log('Error in ' + name + ' helper: ' + e, ERROR);
+        return chunk.setError(e);
       }
-    } catch (err) {
-      chunk.setError(err);
+    } else {
+      dust.log('Invalid helper [' + name + ']', WARN);
       return chunk;
     }
   };
